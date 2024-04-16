7dff3c449538d634ab7f7c7134d35be1830135df
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-16 06:12:38 +0000

    revised profileSchema

--- models/profile.model.js
+++ models/profile.model.js
@@ -1,6 +1,6 @@
 const mongose = require("mongoose");
 
-const xProfileSchema = new mongoose.schema({
+/* const xProfileSchema = new mongoose.schema({
   fullName : String,
   userName: String,
   bio: String, 
@@ -14,4 +14,19 @@ const xProfileSchema = new mongoose.schema({
 
 const Profile = mongoose.model("Profile", xProfileSchema)
 
+module.exports = Profile */
+
+const xProfileSchema = new mongoose.Schema({
+  fullName : String,
+  username : String,
+  bio : String,
+  profilePicUrl : String,
+  followingCount : Number,
+  followerCount : Number,
+  companyName : String,
+  location : String,
+  portfolioUrl : String
+})
+
+const Profile = mongoose.model("Profile", xProfileSchema);
 module.exports = Profile
