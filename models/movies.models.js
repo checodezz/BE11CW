8e8eed41a75b2db97aaa4f30e750f8c402b4fc9b
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-16 06:37:00 +0000

    revised movieSchema

--- models/movies.models.js
+++ models/movies.models.js
@@ -1,8 +1,6 @@
-const { default: mongoose } = require("mongoose");
-
 const mongoose = require("mongoose");
 
-const movieSchema = new mongoose.Schema(
+/* const movieSchema = new mongoose.Schema(
   {
     title: "String",
     required: true,
@@ -66,4 +64,71 @@ const movieSchema = new mongoose.Schema(
 );
 
 const Movie = mongoose.model("Movie", movieSchema);
+module.exports = Movie;
+ */
+
+const moviesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    releaseYear: {
+      type: Number,
+      required: true,
+    },
+    genre: [
+      {
+        type: String,
+        enum: [
+          "Action",
+          "Drama",
+          "Comedy",
+          "Romance",
+          "Thriller",
+          "Fantasy",
+          "Sci-Fi",
+          "Horror",
+          "Sports",
+          "Musical",
+          "Other",
+        ],
+      },
+    ],
+    director: {
+      type: String,
+      required: true,
+    },
+    actors: [
+      {
+        type: String,
+      },
+    ],
+    country: {
+      type: String,
+      default: "India",
+    },
+    rating: {
+      type: String,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    plot: {
+      type: String,
+    },
+    awards: {
+      type: String,
+    },
+    posterUrl: {
+      type: String,
+    },
+    trailerUrl: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const Movie = mongoose.model("Movie", moviesSchema);
 module.exports = Movie;
